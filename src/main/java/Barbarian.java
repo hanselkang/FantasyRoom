e56cfc2d3ccf4a02aebf9b899a66f97b2a237796
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-06-22 11:48:35 +0100

    weapon class

---
+++ src/main/java/Barbarian.java
@@ -0,0 +1,5 @@
+public class Barbarian extends Player {
+    public Barbarian(String name, int mana, int stamina, int health, Weapon activeWeapon) {
+        super(name, mana, stamina, health, activeWeapon);
+    }
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public abstract class Player {
+    private String name;
+    private int mana;
+    private int stamina;
+    private int health;
+    private Weapon activeWeapon;
+    private ArrayList<Weapon> weapons;
+    private ArrayList<Treasure> treasures;
+    private ArrayList<Item> items;
+
+
+    public Player(String name, int mana, int stamina, int health, Weapon activeWeapon) {
+        this.name = name;
+        this.mana = mana;
+        this.stamina = stamina;
+        this.health = health;
+        this.activeWeapon = activeWeapon;
+        weapons = new ArrayList<Weapon>();
+        treasures = new ArrayList<Treasure>();
+        items = new ArrayList<Item>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public Weapon getActiveWeapon() {
+        return activeWeapon;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public ArrayList<Treasure> getTreasures() {
+        return treasures;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+
+}
